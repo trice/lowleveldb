eab129aac3446132621717039ce925d4e3da8cab
Terry Rice <trice7454@gmail.com>
2025-08-06 19:39:44 -0500

    Lesson: getopts + project setup

---
+++ src/main.c
@@ -0,0 +1,39 @@
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void print_usage(char* argv[]) {
+    printf("usage: %s -n -f <filepath>\n", argv[0]);
+    printf("\t -n - create a new file\n");
+    printf("\t -f - (required) file path\n");
+}
+
+int main(int argc, char *argv[]) {
+    bool new_file = false;
+    char* file_path = NULL;
+
+    int c;
+    while ((c = getopt(argc, argv, "nf:")) != -1) {
+        switch (c) {
+            case 'n':
+                new_file = true;
+                break;
+            case 'f':
+                file_path = optarg;
+                break;
+            case '?':
+                printf("unknown option %c\n", c);
+                break;
+            default:
+                return EXIT_FAILURE;
+        }
+    }
+
+    if (file_path == NULL) {
+        print_usage(argv);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
